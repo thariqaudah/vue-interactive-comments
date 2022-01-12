d0c07c94699fcd62a0abb9da4f1801ce27cb940e
Thariq Audah <thariq.audah@gmail.com>
2022-01-12 09:32:24 +0700

    Integrate with firebase

--- src/App.vue
+++ src/App.vue
@@ -1,169 +1,204 @@
 <template>
-	<div class="app">
-		<!-- Comments -->
-		<div class="comments">
-			<ul class="comment-list">
-				<li v-for="comment in comments" :key="comment.id">
-					<SingleComment :comment="comment" />
-				</li>
-			</ul>
-		</div>
+  <div class="app">
+    <!-- Comments -->
+    <div class="comments">
+      <ul class="comment-list">
+        <li v-for="comment in orderedComments" :key="comment.id">
+          <SingleComment :comment="comment" />
+        </li>
+      </ul>
+    </div>
 
-		<!-- Form Comment -->
-		<div class="form-area">
-			<FormComment />
-		</div>
-	</div>
+    <!-- Form Comment -->
+    <div class="form-area">
+      <FormComment />
+    </div>
+  </div>
 </template>
 
 <script>
-import { onMounted, provide, ref } from 'vue';
+import { computed, onMounted, provide, ref } from 'vue';
 import SingleComment from './components/SingleComment.vue';
 import FormComment from './components/FormComment.vue';
 import { db } from './firebase/index';
 import { collection, getDocs } from 'firebase/firestore';
 
 export default {
-	name: 'App',
-	components: {
-		SingleComment,
-		FormComment,
-	},
-	setup() {
-		// const comments = ref([
-		// 	{
-		// 		id: 1,
-		// 		content:
-		// 			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-		// 		createdAt: '1 month ago',
-		// 		score: 12,
-		// 		user: {
-		// 			image: {
-		// 				png: 'image-amyrobson.png',
-		// 				webp: 'image-amyrobson.webp',
-		// 			},
-		// 			username: 'amyrobson',
-		// 		},
-		// 		replies: [],
-		// 	},
-		// 	{
-		// 		id: 2,
-		// 		content:
-		// 			"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-		// 		createdAt: '2 weeks ago',
-		// 		score: 5,
-		// 		user: {
-		// 			image: {
-		// 				png: 'image-maxblagun.png',
-		// 				webp: 'image-maxblagun.webp',
-		// 			},
-		// 			username: 'maxblagun',
-		// 		},
-		// 		replies: [
-		// 			{
-		// 				id: 3,
-		// 				content:
-		// 					"If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-		// 				createdAt: '1 week ago',
-		// 				score: 4,
-		// 				replyingTo: 'maxblagun',
-		// 				user: {
-		// 					image: {
-		// 						png: 'image-ramsesmiron.png',
-		// 						webp: 'image-ramsesmiron.webp',
-		// 					},
-		// 					username: 'ramsesmiron',
-		// 				},
-		// 			},
-		// 			{
-		// 				id: 4,
-		// 				content:
-		// 					"I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-		// 				createdAt: '2 days ago',
-		// 				score: 2,
-		// 				replyingTo: 'ramsesmiron',
-		// 				user: {
-		// 					image: {
-		// 						png: 'image-juliusomo.png',
-		// 						webp: 'image-juliusomo.webp',
-		// 					},
-		// 					username: 'juliusomo',
-		// 				},
-		// 			},
-		// 		],
-		// 	},
-		// ]);
-		const currentUser = ref({
-			image: {
-				png: 'image-juliusomo.png',
-				webp: 'image-juliusomo.webp',
-			},
-			username: 'juliusomo',
-		});
-		const comments = ref(null);
+  name: 'App',
+  components: {
+    SingleComment,
+    FormComment,
+  },
+  setup() {
+    // const comments = ref([
+    // 	{
+    // 		id: 1,
+    // 		content:
+    // 			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+    // 		createdAt: '1 month ago',
+    // 		score: 12,
+    // 		user: {
+    // 			image: {
+    // 				png: 'image-amyrobson.png',
+    // 				webp: 'image-amyrobson.webp',
+    // 			},
+    // 			username: 'amyrobson',
+    // 		},
+    // 		replies: [],
+    // 	},
+    // 	{
+    // 		id: 2,
+    // 		content:
+    // 			"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+    // 		createdAt: '2 weeks ago',
+    // 		score: 5,
+    // 		user: {
+    // 			image: {
+    // 				png: 'image-maxblagun.png',
+    // 				webp: 'image-maxblagun.webp',
+    // 			},
+    // 			username: 'maxblagun',
+    // 		},
+    // 		replies: [
+    // 			{
+    // 				id: 3,
+    // 				content:
+    // 					"If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+    // 				createdAt: '1 week ago',
+    // 				score: 4,
+    // 				replyingTo: 'maxblagun',
+    // 				user: {
+    // 					image: {
+    // 						png: 'image-ramsesmiron.png',
+    // 						webp: 'image-ramsesmiron.webp',
+    // 					},
+    // 					username: 'ramsesmiron',
+    // 				},
+    // 			},
+    // 			{
+    // 				id: 4,
+    // 				content:
+    // 					"I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+    // 				createdAt: '2 days ago',
+    // 				score: 2,
+    // 				replyingTo: 'ramsesmiron',
+    // 				user: {
+    // 					image: {
+    // 						png: 'image-juliusomo.png',
+    // 						webp: 'image-juliusomo.webp',
+    // 					},
+    // 					username: 'juliusomo',
+    // 				},
+    // 			},
+    // 		],
+    // 	},
+    // ]);
 
-		provide('comments', comments);
-		provide('currentUser', currentUser);
+    const currentUser = ref({
+      image: {
+        png: 'image-juliusomo.png',
+        webp: 'image-juliusomo.webp',
+      },
+      username: 'juliusomo',
+    }); // Pretend it come from database
+    const comments = ref([]);
 
-		onMounted(async () => {
-			const querySnapshot = await getDocs(collection(db, 'comments'));
-			querySnapshot.forEach(doc => console.log(doc.data()));
-		});
+    const orderedComments = computed(() => {
+      let ordered = comments.value;
+      return ordered.sort(
+        (firstEl, secondEl) => secondEl.score - firstEl.score
+      );
+    });
 
-		return { currentUser, comments };
-	},
+    provide('currentUser', currentUser);
+
+    const getReplies = async docId => {
+      const repliesRef = await getDocs(
+        collection(db, 'comments', docId, 'replies')
+      );
+      return repliesRef;
+    };
+
+    onMounted(async () => {
+      let data = [];
+
+      const querySnapshot = await getDocs(collection(db, 'comments'));
+      querySnapshot.forEach(async doc => {
+        const data = {
+          id: doc.id,
+          ...doc.data(),
+          replies: [],
+        };
+
+        // Check if comment has a reply
+        const replies = await getReplies(doc.id);
+        if (replies.docs.length) {
+          replies.docs.forEach(reply => {
+            data.replies.push({
+              id: reply.id,
+              ...reply.data(),
+            });
+            // console.log(doc.replies);
+          });
+        }
+        console.log(data);
+      });
+    });
+
+    return { currentUser, comments, orderedComments };
+  },
 };
 </script>
 
 <style lang="scss">
 * {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
 body {
-	font-family: 'Rubik', sans-serif;
-	background-color: #f5f6fa;
-	color: #67727e;
+  font-family: 'Rubik', sans-serif;
+  background-color: #f5f6fa;
+  color: #67727e;
 }
 
 .app {
-	width: 100%;
-	max-width: 1440px;
-	margin: 0 auto;
-	padding: 64px 32px;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 64px 32px;
 }
 
 .btn {
-	display: inline-block;
-	font-size: 16px;
-	background: none;
-	border: none;
-	cursor: pointer;
-	transition: all 0.2s ease;
+  display: inline-block;
+  font-size: 16px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease;
 }
 
 .btn-primary {
-	background-color: #5457b6;
-	color: #fff;
-	padding: 12px 24px;
-	border-radius: 9px;
-	text-transform: uppercase;
-	font-weight: 700;
+  background-color: #5457b6;
+  color: #fff;
+  padding: 12px 24px;
+  border-radius: 9px;
+  text-transform: uppercase;
+  font-weight: 700;
 
-	&:hover {
-		background-color: #c3c4ef;
-	}
+  &:hover {
+    background-color: #c3c4ef;
+  }
 }
 
 .comments,
 .form-area {
-	width: 55%;
-	margin: 0 auto;
+  width: 55%;
+  margin: 0 auto;
 }
 
 .comment-list {
-	list-style: none;
+  list-style: none;
 }
 </style>
--- src/components/SingleComment.vue
+++ src/components/SingleComment.vue
@@ -1,75 +1,75 @@
 <template>
-	<div class="sigle-comment">
-		<!-- Main comment -->
-		<div class="comment">
-			<div class="comment-vote">
-				<form>
-					<button class="btn btn-upvote" type="button" @click="upVote">
-						<i class="fas fa-plus icon-vote"></i>
-					</button>
-					<input type="number" v-model="vote" />
-					<button class="btn btn-downvote" type="button" @click="downVote">
-						<i class="fas fa-minus icon-vote"></i>
-					</button>
-				</form>
-			</div>
-			<div class="comment-main">
-				<div class="comment-header">
-					<div class="user-profile">
-						<img
-							class="avatar"
-							:src="require(`../assets/avatars/${comment.user.image.png}`)"
-							alt="user image"
-						/>
-						<p class="username">{{ comment.user.username }}</p>
-					</div>
-					<div class="label" v-if="ownership">you</div>
-					<p class="created-at">{{ comment.createdAt }}</p>
-					<div class="action">
-						<div v-if="ownership">
-							<button class="btn btn-delete">
-								<i class="fas fa-trash"></i>
-								<span>Delete</span>
-							</button>
-							<button class="btn btn-edit">
-								<i class="fas fa-pen"></i>
-								<span>Edit</span>
-							</button>
-						</div>
-						<button class="btn btn-reply" v-else @click="showForm = true">
-							<i class="fas fa-reply"></i>
-							<span>Reply</span>
-						</button>
-					</div>
-				</div>
+  <div class="sigle-comment">
+    <!-- Main comment -->
+    <div class="comment">
+      <div class="comment-vote">
+        <form>
+          <button class="btn btn-upvote" type="button" @click="upVote">
+            <i class="fas fa-plus icon-vote"></i>
+          </button>
+          <input type="number" v-model="vote" />
+          <button class="btn btn-downvote" type="button" @click="downVote">
+            <i class="fas fa-minus icon-vote"></i>
+          </button>
+        </form>
+      </div>
+      <div class="comment-main">
+        <div class="comment-header">
+          <div class="user-profile">
+            <img
+              class="avatar"
+              :src="require(`../assets/avatars/${comment.user.image.png}`)"
+              alt="user image"
+            />
+            <p class="username">{{ comment.user.username }}</p>
+          </div>
+          <div class="label" v-if="ownership">you</div>
+          <p class="created-at">{{ comment.createdAt.seconds }}</p>
+          <div class="action">
+            <div v-if="ownership">
+              <button class="btn btn-delete">
+                <i class="fas fa-trash"></i>
+                <span>Delete</span>
+              </button>
+              <button class="btn btn-edit">
+                <i class="fas fa-pen"></i>
+                <span>Edit</span>
+              </button>
+            </div>
+            <button class="btn btn-reply" v-else @click="showForm = true">
+              <i class="fas fa-reply"></i>
+              <span>Reply</span>
+            </button>
+          </div>
+        </div>
 
-				<div class="comment-body">
-					<p>
-						<span class="replying-to" v-if="comment.replyingTo"
-							>@{{ comment.replyingTo }}</span
-						>
-						{{ comment.content }}
-					</p>
-				</div>
-			</div>
-		</div>
+        <div class="comment-body">
+          <p>
+            <span class="replying-to" v-if="comment.replyingTo"
+              >@{{ comment.replyingTo }}</span
+            >
+            {{ comment.content }}
+          </p>
+        </div>
+      </div>
+    </div>
 
-		<!-- Input form comment -->
-		<FormComment
-			:class="{ 'form-active': showForm }"
-			:replyingTo="comment"
-			v-if="showForm"
-		/>
+    <!-- Input form comment -->
+    <FormComment
+      :class="{ 'form-active': showForm }"
+      :replyingTo="comment"
+      v-if="showForm"
+    />
 
-		<!-- Comment replies -->
-		<div class="replies" v-if="comment.replies">
-			<ul class="comment-list">
-				<li v-for="reply in comment.replies" :key="reply.id">
-					<SingleComment :comment="reply" />
-				</li>
-			</ul>
-		</div>
-	</div>
+    <!-- Comment replies -->
+    <div class="replies" v-if="comment.replies">
+      <ul class="comment-list">
+        <li v-for="reply in comment.replies" :key="reply.id">
+          <SingleComment :comment="reply" />
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -78,186 +78,186 @@ import SingleComment from './SingleComment.vue';
 import FormComment from './FormComment.vue';
 
 export default {
-	name: 'Comment',
-	components: { SingleComment, FormComment },
-	props: ['comment'],
-	setup(props) {
-		const vote = ref(props.comment.score);
-		const showForm = ref(null);
+  name: 'Comment',
+  components: { SingleComment, FormComment },
+  props: ['comment'],
+  setup(props) {
+    const vote = ref(props.comment.score);
+    const showForm = ref(null);
 
-		const currentUser = inject('currentUser');
+    const currentUser = inject('currentUser');
 
-		const ownership = computed(() => {
-			return props.comment.user.username === currentUser.value.username;
-		});
+    const ownership = computed(() => {
+      return props.comment.user.username === currentUser.value.username;
+    });
 
-		const upVote = () => {
-			vote.value++;
-		};
+    const upVote = () => {
+      vote.value++;
+    };
 
-		const downVote = () => {
-			vote.value--;
-		};
+    const downVote = () => {
+      vote.value--;
+    };
 
-		return { vote, showForm, ownership, upVote, downVote };
-	},
+    return { vote, showForm, ownership, upVote, downVote };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .comment {
-	background-color: #fff;
-	display: flex;
-	gap: 32px;
-	border-radius: 9px;
-	padding: 24px;
-	margin-bottom: 24px;
+  background-color: #fff;
+  display: flex;
+  gap: 32px;
+  border-radius: 9px;
+  padding: 24px;
+  margin-bottom: 24px;
 
-	.comment-vote {
-		form {
-			background-color: #eaecf1;
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			justify-content: center;
-			gap: 16px;
-			width: 48px;
-			padding: 8px;
-			border-radius: 9px;
-		}
+  .comment-vote {
+    form {
+      background-color: #eaecf1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 16px;
+      width: 48px;
+      padding: 8px;
+      border-radius: 9px;
+    }
 
-		input[type='number']::-webkit-inner-spin-button,
-		input[type='number']::-webkit-outer-spin-button {
-			-webkit-appearance: none;
-			margin: 0;
-		}
+    input[type='number']::-webkit-inner-spin-button,
+    input[type='number']::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
 
-		input[type='number'] {
-			-moz-appearance: textfield;
-		}
+    input[type='number'] {
+      -moz-appearance: textfield;
+    }
 
-		input[type='number'] {
-			display: inline-block;
-			background-color: #eaecf1;
-			width: 100%;
-			border: none;
-			outline: none;
-			padding: 0;
-			margin: 0;
-			text-align: center;
-			font-size: 18px;
-			font-family: inherit;
-			font-weight: 500;
-			color: #5457b6;
-			pointer-events: none;
-		}
+    input[type='number'] {
+      display: inline-block;
+      background-color: #eaecf1;
+      width: 100%;
+      border: none;
+      outline: none;
+      padding: 0;
+      margin: 0;
+      text-align: center;
+      font-size: 18px;
+      font-family: inherit;
+      font-weight: 500;
+      color: #5457b6;
+      pointer-events: none;
+    }
 
-		.icon-vote {
-			font-size: 14px;
-			color: #c3c4ef;
-			transition: all 0.2s ease;
-		}
+    .icon-vote {
+      font-size: 14px;
+      color: #c3c4ef;
+      transition: all 0.2s ease;
+    }
 
-		.btn-upvote:hover .icon-vote {
-			color: #5457b6;
-		}
+    .btn-upvote:hover .icon-vote {
+      color: #5457b6;
+    }
 
-		.btn-downvote:hover .icon-vote {
-			color: #5457b6;
-		}
-	}
+    .btn-downvote:hover .icon-vote {
+      color: #5457b6;
+    }
+  }
 
-	.comment-main {
-		width: 100%;
-		.comment-header {
-			display: flex;
-			gap: 12px;
-			align-items: center;
-			margin-bottom: 16px;
+  .comment-main {
+    width: 100%;
+    .comment-header {
+      display: flex;
+      gap: 12px;
+      align-items: center;
+      margin-bottom: 16px;
 
-			.user-profile {
-				display: flex;
-				align-items: center;
-				gap: 12px;
+      .user-profile {
+        display: flex;
+        align-items: center;
+        gap: 12px;
 
-				.avatar {
-					width: 40px;
-					height: 40px;
-				}
+        .avatar {
+          width: 40px;
+          height: 40px;
+        }
 
-				.username {
-					font-weight: 500;
-					color: #324152;
-				}
-			}
+        .username {
+          font-weight: 500;
+          color: #324152;
+        }
+      }
 
-			.label {
-				font-size: 14px;
-				background-color: #5457b6;
-				color: #fff;
-				padding: 2px 8px;
-				border-radius: 3px;
-			}
+      .label {
+        font-size: 14px;
+        background-color: #5457b6;
+        color: #fff;
+        padding: 2px 8px;
+        border-radius: 3px;
+      }
 
-			.created-at {
-				color: #67727e;
-			}
+      .created-at {
+        color: #67727e;
+      }
 
-			.action {
-				margin-left: auto;
+      .action {
+        margin-left: auto;
 
-				div:first-child {
-					display: flex;
-					gap: 16px;
-				}
+        div:first-child {
+          display: flex;
+          gap: 16px;
+        }
 
-				.btn-reply,
-				.btn-edit,
-				.btn-delete {
-					span {
-						font-weight: 700;
-						margin-left: 8px;
-					}
-				}
+        .btn-reply,
+        .btn-edit,
+        .btn-delete {
+          span {
+            font-weight: 700;
+            margin-left: 8px;
+          }
+        }
 
-				.btn-reply,
-				.btn-edit {
-					color: #5457b6;
+        .btn-reply,
+        .btn-edit {
+          color: #5457b6;
 
-					&:hover {
-						color: #c3c4ef;
-					}
-				}
+          &:hover {
+            color: #c3c4ef;
+          }
+        }
 
-				.btn-delete {
-					color: #ed6468;
+        .btn-delete {
+          color: #ed6468;
 
-					&:hover {
-						color: #ffb8bb;
-					}
-				}
-			}
-		}
+          &:hover {
+            color: #ffb8bb;
+          }
+        }
+      }
+    }
 
-		.comment-body {
-			line-height: 1.6;
+    .comment-body {
+      line-height: 1.6;
 
-			.replying-to {
-				font-weight: 500;
-				color: #5457b6;
-			}
-		}
-	}
+      .replying-to {
+        font-weight: 500;
+        color: #5457b6;
+      }
+    }
+  }
 }
 
 .form-active {
-	margin-bottom: 24px;
-	margin-top: -12px;
+  margin-bottom: 24px;
+  margin-top: -12px;
 }
 
 .replies {
-	margin-left: 32px;
-	padding-left: 32px;
-	border-left: 2px solid #eaecf1;
+  margin-left: 32px;
+  padding-left: 32px;
+  border-left: 2px solid #eaecf1;
 }
 </style>
