21bb279937be455ded15aa7df386661097118ec4
Thariq Audah <thariq.audah@gmail.com>
2022-01-18 07:02:58 +0700

    Finish CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,35 @@
 <template>
-	<div class="app">
-		<!-- Comments -->
-		<div class="comments">
-			<ul class="comment-list">
-				<li v-for="comment in orderedComments" :key="comment.id">
-					<SingleComment
-						:comment="comment"
-						@reply="handleReply"
-						@delete="handleDelete(comment.id)"
-					/>
-					<!-- Input form comment -->
-					<FormComment
-						class="form-active"
-						:replyingTo="comment.user.username"
-						:parentComment="comment.id"
-						v-if="showForm && comment.id === commentId"
-					/>
+  <div class="app">
+    <!-- Comments -->
+    <div class="comments">
+      <ul class="comment-list">
+        <li v-for="comment in orderedComments" :key="comment.id">
+          <SingleComment :comment="comment" :parentCommentId="comment.id" />
 
-					<!-- Comment replies -->
-					<div class="replies" v-if="comment.replies">
-						<ul class="comment-list">
-							<li v-for="reply in comment.replies" :key="reply.id">
-								<SingleComment
-									:comment="reply"
-									:parentComment="comment.id"
-									@reply="handleReply"
-									@delete="handleDelete(comment.id, reply.id)"
-								/>
-								<!-- Input form comment -->
-								<FormComment
-									class="form-active"
-									:replyingTo="reply.user.username"
-									:parentComment="comment.id"
-									v-if="showForm && reply.id === commentId"
-								/>
-							</li>
-						</ul>
-					</div>
-				</li>
-			</ul>
-		</div>
+          <!-- Comment replies -->
+          <div class="replies" v-if="comment.replies">
+            <ul class="comment-list">
+              <li v-for="reply in comment.replies" :key="reply.id">
+                <SingleComment :comment="reply" :parentCommentId="comment.id" />
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
 
-		<!-- Form Comment -->
-		<div class="form-area">
-			<FormComment />
-		</div>
-	</div>
+    <!-- Form Comment -->
+    <div class="form-container">
+      <div class="user-profile">
+        <img
+          class="avatar"
+          :src="require(`./assets/avatars/${currentUser.image.png}`)"
+          alt="Current user photo"
+        />
+      </div>
+      <FormComment />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -54,208 +38,222 @@ import SingleComment from './components/SingleComment.vue';
 import FormComment from './components/FormComment.vue';
 import { db } from './firebase/index';
 import {
-	collection,
-	doc,
-	getDocs,
-	getDoc,
-	query,
-	orderBy,
-	deleteDoc,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  query,
+  orderBy,
 } from 'firebase/firestore';
 
 export default {
-	name: 'App',
-	components: {
-		SingleComment,
-		FormComment,
-	},
-	setup() {
-		const comments = ref([]);
-		const currentUser = ref({
-			image: {
-				png: 'image-juliusomo.png',
-				webp: 'image-juliusomo.webp',
-			},
-			username: 'juliusomo',
-		}); // Pretend it comes from database
-		const showForm = ref(null);
-		const commentId = ref(null);
+  name: 'App',
+  components: {
+    SingleComment,
+    FormComment,
+  },
+  setup() {
+    const comments = ref([]);
+    const currentUser = ref({
+      image: {
+        png: 'image-juliusomo.png',
+        webp: 'image-juliusomo.webp',
+      },
+      username: 'juliusomo',
+    }); // Pretend it comes from database
+    const showForm = ref(null);
+    const commentId = ref(null);
 
-		const orderedComments = computed(() => {
-			let ordered = comments.value;
-			return ordered.sort(
-				(firstEl, secondEl) => secondEl.score - firstEl.score
-			);
-		});
+    const orderedComments = computed(() => {
+      let ordered = comments.value;
+      return ordered.sort(
+        (firstEl, secondEl) => secondEl.score - firstEl.score
+      );
+    });
 
-		const handleReply = id => {
-			showForm.value = true;
-			commentId.value = id;
-		};
+    // Handle UI
+    const addComment = async commentId => {
+      const commentSnap = await getDoc(doc(db, 'comments', commentId));
+      comments.value.push({
+        id: commentSnap.id,
+        ...commentSnap.data(),
+        replies: [],
+      });
+    };
 
-		const handleDelete = async (commentId, replyId) => {
-			if (!replyId) {
-				await deleteDoc(doc(db, 'comments', commentId));
-				console.log('Comment deleted');
-				removeComment(commentId);
-				return;
-			}
+    const addReply = async (commentId, newReply) => {
+      const comment = comments.value.find(comm => comm.id === commentId);
+      if (comment) {
+        comment.replies.push(newReply);
+      }
+    };
 
-			await deleteDoc(doc(db, 'comments', commentId, 'replies', replyId));
-			console.log('Comment deleted');
-			removeComment(commentId, replyId);
-		};
+    const removeComment = (commentId, replyId) => {
+      if (!replyId) {
+        comments.value = comments.value.filter(
+          comment => comment.id !== commentId
+        );
+        return;
+      }
 
-		// Handle UI
-		const addComment = async commentId => {
-			const commentSnap = await getDoc(doc(db, 'comments', commentId));
-			comments.value.push({
-				id: commentSnap.id,
-				...commentSnap.data(),
-				replies: [],
-			});
-		};
+      comments.value = comments.value.map(comment => {
+        if (comment.id === commentId) {
+          comment.replies = comment.replies.filter(
+            reply => reply.id !== replyId
+          );
+          return comment;
+        }
 
-		const addReply = async (commentId, replyId) => {
-			showForm.value = null;
+        return comment;
+      });
+    };
 
-			const replySnap = await getDoc(
-				doc(db, 'comments', commentId, 'replies', replyId)
-			);
-			const comment = comments.value.find(comm => comm.id === commentId);
-			if (comment) {
-				comment.replies.push({
-					id: replySnap.id,
-					...replySnap.data(),
-				});
-			}
-		};
+    const editComment = (commentId, replyId, updatedContent) => {
+      console.log('fired');
+      if (commentId === replyId) {
+        comments.value = comments.value.map(comment => {
+          if (comment.id === commentId) {
+            comment.content = updatedContent;
+          }
+          return comment;
+        });
+        return;
+      }
 
-		const removeComment = (commentId, replyId) => {
-			if (!replyId) {
-				console.log('fired');
-				comments.value = comments.value.filter(
-					comment => comment.id !== commentId
-				);
-				return;
-			}
+      comments.value = comments.value.map(comment => {
+        if (comment.id === commentId) {
+          comment.replies = comment.replies.map(reply => {
+            if (reply.id === replyId) {
+              reply.content = updatedContent;
+            }
+            return reply;
+          });
+          return comment;
+        }
 
-			comments.value = comments.value.map(comment => {
-				if (comment.id === commentId) {
-					comment.replies = comment.replies.filter(
-						reply => reply.id !== replyId
-					);
-					return comment;
-				}
+        return comment;
+      });
+    };
 
-				return comment;
-			});
-		};
+    onMounted(async () => {
+      const querySnapshot = await getDocs(collection(db, 'comments'));
+      querySnapshot.forEach(async doc => {
+        const comment = {
+          id: doc.id,
+          ...doc.data(),
+          replies: [],
+        };
 
-		onMounted(async () => {
-			const querySnapshot = await getDocs(collection(db, 'comments'));
-			querySnapshot.forEach(async doc => {
-				const comment = {
-					id: doc.id,
-					...doc.data(),
-					replies: [],
-				};
+        // Check if comment has replies
+        const repliesRef = collection(db, 'comments', doc.id, 'replies');
+        const queryReplies = query(repliesRef, orderBy('createdAt'));
+        const repliesSnap = await getDocs(queryReplies);
+        if (repliesSnap.docs.length > 0) {
+          repliesSnap.docs.forEach(reply => {
+            comment.replies.push({
+              id: reply.id,
+              ...reply.data(),
+            });
+          });
+        }
 
-				// Check if comment has replies
-				const repliesRef = collection(db, 'comments', doc.id, 'replies');
-				const queryReplies = query(repliesRef, orderBy('createdAt'));
-				const repliesSnap = await getDocs(queryReplies);
-				if (repliesSnap.docs.length > 0) {
-					repliesSnap.docs.forEach(reply => {
-						comment.replies.push({
-							id: reply.id,
-							...reply.data(),
-						});
-					});
-				}
+        // Push to comments state
+        comments.value.push(comment);
+      });
+    });
 
-				// Push to comments state
-				comments.value.push(comment);
-			});
-		});
+    provide('currentUser', currentUser);
+    provide('addComment', addComment);
+    provide('addReply', addReply);
+    provide('removeComment', removeComment);
+    provide('editComment', editComment);
 
-		provide('currentUser', currentUser);
-		provide('addComment', addComment);
-		provide('addReply', addReply);
-
-		return {
-			currentUser,
-			comments,
-			showForm,
-			commentId,
-			orderedComments,
-			handleReply,
-			handleDelete,
-		};
-	},
+    return {
+      currentUser,
+      comments,
+      showForm,
+      commentId,
+      orderedComments,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 * {
-	padding: 0;
-	margin: 0;
-	box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 
 body {
-	font-family: 'Rubik', sans-serif;
-	background-color: #f5f6fa;
-	color: #67727e;
+  font-family: 'Rubik', sans-serif;
+  background-color: #f5f6fa;
+  color: #67727e;
 }
 
 .app {
-	width: 100%;
-	max-width: 1440px;
-	margin: 0 auto;
-	padding: 64px 32px;
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 64px 32px;
 }
 
 .btn {
-	display: inline-block;
-	font-size: 16px;
-	background: none;
-	border: none;
-	cursor: pointer;
-	transition: all 0.2s ease;
+  display: inline-block;
+  font-size: 16px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease;
 }
 
 .btn-primary {
-	background-color: #5457b6;
-	color: #fff;
-	padding: 12px 24px;
-	border-radius: 9px;
-	text-transform: uppercase;
-	font-weight: 700;
+  background-color: #5457b6;
+  color: #fff;
+  padding: 12px 24px;
+  border-radius: 9px;
+  text-transform: uppercase;
+  font-weight: 700;
 
-	&:hover {
-		background-color: #c3c4ef;
-	}
+  &:hover {
+    background-color: #c3c4ef;
+  }
 }
 
 .comments,
-.form-area {
-	width: 55%;
-	margin: 0 auto;
+.form-container {
+  width: 55%;
+  margin: 0 auto;
 }
 
 .comment-list {
-	list-style: none;
+  list-style: none;
 }
 
 .replies {
-	margin-left: 32px;
-	padding-left: 32px;
-	border-left: 2px solid hsl(223, 19%, 93%);
+  margin-left: 32px;
+  padding-left: 32px;
+  border-left: 2px solid hsl(223, 19%, 93%);
 }
 
-.form-active {
-	margin-bottom: 24px;
-	margin-top: -12px;
+.form-container {
+  background-color: #fff;
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+  border-radius: 9px;
+  padding: 24px;
+
+  .user-profile {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .avatar {
+      width: 50px;
+      height: 50px;
+    }
+  }
 }
 </style>
--- src/components/FormComment.vue
+++ src/components/FormComment.vue
@@ -1,198 +1,238 @@
 <template>
-	<div class="form-comment">
-		<div class="user-profile">
-			<img
-				class="avatar"
-				:src="require(`../assets/avatars/${currentUser.image.png}`)"
-				alt="Current user photo"
-			/>
-		</div>
-
-		<form class="form">
-			<textarea
-				placeholder="Add a comment..."
-				v-model="commentMessage"
-			></textarea>
-			<button
-				class="btn btn-primary"
-				type="submit"
-				v-if="replyingTo"
-				@click.prevent="sendReply"
-			>
-				Reply
-			</button>
-			<button
-				class="btn btn-primary"
-				type="submit"
-				v-else
-				@click.prevent="sendComment"
-			>
-				Send
-			</button>
-		</form>
-	</div>
+  <form class="form-comment">
+    <textarea
+      placeholder="Add a comment..."
+      v-model="commentMessage"
+    ></textarea>
+    <button
+      class="btn btn-primary"
+      type="submit"
+      v-if="comment"
+      @click.prevent="updateComment"
+    >
+      Update
+    </button>
+    <button
+      class="btn btn-primary"
+      type="submit"
+      v-if="replyingTo"
+      @click.prevent="sendReply"
+    >
+      Reply
+    </button>
+    <button
+      class="btn btn-primary"
+      type="submit"
+      v-if="!replyingTo && !comment"
+      @click.prevent="sendComment"
+    >
+      Send
+    </button>
+  </form>
 </template>
 
 <script>
 import { inject, onMounted, ref } from 'vue';
-import { addDoc, getDoc, collection, doc, Timestamp } from 'firebase/firestore';
+import {
+  doc,
+  collection,
+  addDoc,
+  updateDoc,
+  Timestamp,
+} from 'firebase/firestore';
 import { db } from '../firebase/index';
 
 export default {
-	name: 'FormComment',
-	props: ['replyingTo', 'parentComment'],
-	setup(props) {
-		const commentMessage = ref(null);
-		const error = ref(null);
-		const errMessage = ref(null);
+  name: 'FormComment',
+  props: ['replyingTo', 'parentCommentId', 'comment'],
+  setup(props, { emit }) {
+    const commentMessage = ref(null);
+    const error = ref(null);
+    const errMessage = ref(null);
 
-		const currentUser = inject('currentUser');
-		const addComment = inject('addComment');
-		const addReply = inject('addReply');
+    const currentUser = inject('currentUser');
+    const addComment = inject('addComment');
+    const addReply = inject('addReply');
+    const editComment = inject('editComment');
 
-		const sendComment = async () => {
-			error.value = false;
-			errMessage.value = null;
+    const sendComment = async () => {
+      error.value = false;
+      errMessage.value = null;
 
-			if (!commentMessage.value) {
-				error.value = true;
-				errMessage.value = 'Please add a comment';
-				return;
-			}
+      if (!commentMessage.value) {
+        error.value = true;
+        errMessage.value = 'Please add a comment';
+        return;
+      }
 
-			const newComment = {
-				content: commentMessage.value,
-				createdAt: Timestamp.now(),
-				score: 0,
-				user: {
-					image: {
-						png: currentUser.value.image.png,
-						webp: currentUser.value.image.webp,
-					},
-					username: currentUser.value.username,
-				},
-			};
+      const newComment = {
+        content: commentMessage.value,
+        createdAt: Timestamp.now(),
+        score: 0,
+        user: {
+          image: {
+            png: currentUser.value.image.png,
+            webp: currentUser.value.image.webp,
+          },
+          username: currentUser.value.username,
+        },
+      };
 
-			try {
-				const commentSnap = await addDoc(
-					collection(db, 'comments'),
-					newComment
-				);
-				if (commentSnap.id) {
-					commentMessage.value = null;
-					addComment(commentSnap.id);
-				} else {
-					throw new Error('Failed add new comment');
-				}
-			} catch (err) {
-				console.log(err);
-				error.value = true;
-				errMessage.value = err;
-			}
-		};
+      try {
+        const commentSnap = await addDoc(
+          collection(db, 'comments'),
+          newComment
+        );
+        if (commentSnap.id) {
+          commentMessage.value = null;
+          addComment(commentSnap.id);
+        } else {
+          throw new Error('Failed add new comment');
+        }
+      } catch (err) {
+        console.log(err);
+        error.value = true;
+        errMessage.value = err;
+      }
+    };
 
-		const sendReply = async () => {
-			error.value = false;
-			errMessage.value = null;
+    const sendReply = async () => {
+      error.value = false;
+      errMessage.value = null;
 
-			if (!commentMessage.value) {
-				error.value = true;
-				errMessage.value = 'Please add a comment';
-				return;
-			}
+      if (!commentMessage.value) {
+        error.value = true;
+        errMessage.value = 'Please add a comment';
+        return;
+      }
 
-			// Remove replyingTo string in comment message
-			const usernameLength = props.replyingTo.length;
-			const startIndex = usernameLength + 3; // 3 for accounting @, coma, and space
-			commentMessage.value = commentMessage.value.slice(startIndex);
+      // Remove replyingTo string in comment message
+      const usernameLength = props.replyingTo.length;
+      const startIndex = usernameLength + 3; // 3 for accounting @, coma, and space
+      commentMessage.value = commentMessage.value.slice(startIndex);
 
-			const newReply = {
-				content: commentMessage.value,
-				createdAt: Timestamp.now(),
-				score: 0,
-				replyingTo: props.replyingTo,
-				user: {
-					image: {
-						png: currentUser.value.image.png,
-						webp: currentUser.value.image.webp,
-					},
-					username: currentUser.value.username,
-				},
-			};
+      const newReply = {
+        content: commentMessage.value,
+        createdAt: Timestamp.now(),
+        score: 0,
+        replyingTo: props.replyingTo,
+        user: {
+          image: {
+            png: currentUser.value.image.png,
+            webp: currentUser.value.image.webp,
+          },
+          username: currentUser.value.username,
+        },
+      };
 
-			try {
-				// const replyRef = await addDoc(collection(db, ));
-				const commentSnap = await getDoc(
-					doc(db, 'comments', props.parentComment)
-				);
-				const replySnap = await addDoc(
-					collection(db, 'comments', commentSnap.id, 'replies'),
-					newReply
-				);
-				if (replySnap.id) {
-					commentMessage.value = null;
-					addReply(commentSnap.id, replySnap.id);
-				}
-			} catch (err) {
-				console.log(err);
-				error.value = err;
-			}
-		};
+      try {
+        const replyRef = collection(
+          db,
+          'comments',
+          props.parentCommentId,
+          'replies'
+        );
+        const replySnap = await addDoc(replyRef, newReply);
+        console.log(replySnap);
+        if (replySnap.id) {
+          emit('closeForm');
+          commentMessage.value = null;
+          addReply(props.parentCommentId, {
+            id: replySnap.id,
+            ...newReply,
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        error.value = err;
+      }
+    };
 
-		onMounted(() => {
-			if (props.replyingTo) {
-				commentMessage.value = `@${props.replyingTo}, `;
-			}
-		});
+    const updateComment = async () => {
+      let commentRef;
 
-		return { commentMessage, currentUser, sendComment, sendReply };
-	},
+      // Check if comment is a reply or not and remove a replyingTo string
+      if (props.comment.replyingTo) {
+        // Remove replyingTo string in comment message
+        const usernameLength = props.comment.replyingTo.length;
+        const startIndex = usernameLength + 3; // 3 for accounting @, coma, and space
+        commentMessage.value = commentMessage.value.slice(startIndex);
+
+        commentRef = doc(
+          db,
+          'comments',
+          props.parentCommentId,
+          'replies',
+          props.comment.id
+        );
+      } else {
+        commentRef = doc(db, 'comments', props.comment.id);
+      }
+
+      await updateDoc(commentRef, {
+        content: commentMessage.value,
+      });
+      emit('closeEditForm');
+
+      // Call function on App component
+      editComment(
+        props.parentCommentId,
+        props.comment.id,
+        commentMessage.value
+      );
+    };
+
+    onMounted(() => {
+      if (props.replyingTo) {
+        commentMessage.value = `@${props.replyingTo}, `;
+        return;
+      }
+
+      if (props.comment) {
+        if (props.comment.replyingTo) {
+          commentMessage.value = `@${props.comment.replyingTo}, ${props.comment.content}`;
+          return;
+        }
+
+        commentMessage.value = props.comment.content;
+        return;
+      }
+    });
+
+    return {
+      commentMessage,
+      currentUser,
+      sendComment,
+      sendReply,
+      updateComment,
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .form-comment {
-	background-color: #fff;
-	display: flex;
-	align-items: flex-start;
-	gap: 16px;
-	border-radius: 9px;
-	padding: 24px;
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
 
-	.user-profile {
-		display: flex;
-		justify-content: center;
-		align-items: center;
+  textarea {
+    flex: 1;
+    background-color: none;
+    border: 1px solid #eaecf1;
+    font-size: 16px;
+    font-family: inherit;
+    color: #324152;
+    padding: 12px 16px;
+    min-height: 100px;
+    border-radius: 9px;
+    resize: none;
 
-		.avatar {
-			width: 50px;
-			height: 50px;
-		}
-	}
-
-	.form {
-		width: 100%;
-		display: flex;
-		align-items: flex-start;
-		gap: 16px;
-
-		textarea {
-			flex: 1;
-			background-color: none;
-			border: 1px solid #eaecf1;
-			font-size: 16px;
-			font-family: inherit;
-			color: #324152;
-			padding: 12px 16px;
-			min-height: 100px;
-			border-radius: 9px;
-			resize: none;
-
-			&:focus {
-				outline: none;
-				border-color: #324152;
-			}
-		}
-	}
+    &:focus {
+      outline: none;
+      border-color: #324152;
+    }
+  }
 }
 </style>
